68085f79dffd80484231ad9fbbe4e31e6e2ad22e
Max Soe <maxsoe@gmail.com>
2017-11-24 14:11:17 +0000

    6-9. Applying the ng-class Directive to the app.html File

--- chapter06/controllers/productListControllers.js
+++ chapter06/controllers/productListControllers.js
@@ -1,5 +1,5 @@
 angular.module("sportsStore")
-.constant("productListActiveClass", "btn-primary")
+  .constant("productListActiveClass", "btn-primary")
   .controller("productListCtrl", function($scope, $filter, productListActiveClass) {
     var selectedCategory = null;
     $scope.selectCategory = function(newCategory) {
@@ -9,7 +9,7 @@ angular.module("sportsStore")
       return selectedCategory == null ||
         product.category == selectedCategory;
     }
-    $scope.getCategoryClass = function (category) {
+    $scope.getCategoryClass = function(category) {
       return selectedCategory == category ? productListActiveClass : "";
     }
   });
